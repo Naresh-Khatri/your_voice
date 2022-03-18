fa7d5b07a8a6def28902bfa28767ec75b09f8a7c
Naresh <naresh.khatri2345@gmail.com>
2022-03-18 23:49:45 +0530

    SpeakCard component to so text and speak button

--- src/components/SpeakCard.vue
+++ src/components/SpeakCard.vue
@@ -17,18 +17,27 @@
   </div>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, useSlots } from "vue";
+import { useStore } from "vuex";
+import { useQuasar } from "quasar";
 
 const props = defineProps({ id: Number, text: String });
+
+const $q = useQuasar();
+const $store = useStore();
+
 onMounted(() => {
-  console.log(props);
+  setTimeout(() => {
+    // console.log($store.state.voice, speechSynthesis.getVoices()[1]);
+  }, 1000);
 });
 
 const speak = () => {
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = props.text;
-  utterance.voice = speechSynthesis.getVoices()[props.id];
-  utterance.rate = 1;
+  utterance.voice = $store.state.voice || speechSynthesis.getVoices()[props.id];
+  // utterance.voice = speechSynthesis.getVoices()[props.id];
+  utterance.rate = $store.state.rate;
   console.log(utterance);
   console.log(speechSynthesis);
   console.log(speechSynthesis.getVoices());
@@ -38,7 +47,6 @@ const speak = () => {
 <style>
 .container {
   width: 100%;
-  /* background: #298cff; */
   background-color: #000000;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23220000' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23440000' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23660000' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23880000' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23A00' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
   background-attachment: fixed;
