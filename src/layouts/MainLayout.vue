e4c61115327a32d5648ea508c28ee364f80a1b1b
Naresh <naresh.khatri2345@gmail.com>
2022-03-20 19:49:51 +0530

    Remove drawer, change hearder name, add footer tabs for tts and recognition

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,23 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
-        <q-btn
+        <!-- <q-btn
           flat
           dense
           round
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
-        />
+        /> -->
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Your voice </q-toolbar-title>
 
         <!-- <div>Quasar v{{ $q.version }}</div> -->
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
 
@@ -27,81 +27,42 @@
           v-bind="link"
         />
       </q-list>
-    </q-drawer>
+    </q-drawer> -->
 
     <q-page-container>
+      <q-footer class="text-white">
+        <q-toolbar style="padding: 0">
+          <q-toolbar-title>
+            <q-tabs v-model="tab" class="bg-primary bottom-nav">
+              <q-route-tab to="/" icon="fas fa-volume-up" />
+              <q-route-tab to="/recog" icon="fas fa-microphone" />
+            </q-tabs>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script>
-import EssentialLink from "components/EssentialLink.vue";
+<script setup>
+import { ref } from "vue";
 
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
+const leftDrawerOpen = ref(false);
+const tab = ref("");
 
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
-  name: "MainLayout",
-
-  components: {
-    EssentialLink,
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
-  },
-});
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
 </script>
+<style>
+.bottom-nav {
+  border-radius: 20px 20px 0px 0px;
+  position: absolute;
+  display: flex;
+  justify-content: space-evenly;
+  bottom: 0;
+  height: 7vh;
+  width: 100%;
+}
+</style>
